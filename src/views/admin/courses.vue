0c3dbb4dad6927de4eb576a4e8d905fc9c306dc9
Mohammad Sadeghian <mosa5445@gmail.com>
2019-04-17 01:07:24 +0430

    pagination completed

--- src/views/admin/courses.vue
+++ src/views/admin/courses.vue
@@ -5,35 +5,34 @@
       <adminMenu></adminMenu>
       <div class="container-fluid">
         <div class="com-lg-8 col-md-10 col-sm-12" id="list">
-          <router-link  tag="button" class="newcourse" to="/">ایجاد دوره جدید</router-link>
-          <button class="newcourse" @click="page(2)">2</button>
+          <router-link tag="button" class="newcourse" to="/">ایجاد دوره جدید</router-link>
           <div class="course" v-for="course in courses" :key="course._id">
             <div class="d-flex justify-content-between align-items-center flex-wrap">
               <div
-                class="d-flex align-items-center justify-content-around  flex-wrap col-lg-10 col-12"
+                class="d-flex align-items-center justify-content-around flex-wrap col-lg-10 col-12"
               >
-                <div class="item col-3">
+                <div class="item col-md-4 col-sm-12">
                   <div class="d-flex flex-column">
                     <span class="title">نام دوره</span>
                     <span class="info">{{course.title}}</span>
                   </div>
                 </div>
 
-                <div class="item col-2">
+                <div class="item col-md-2 col-sm-4">
                   <div class="d-flex flex-column">
                     <span class="title">نوع دوره</span>
                     <span class="info">{{ course.type }}</span>
                   </div>
                 </div>
 
-                <div class="item col-2">
+                <div class="item col-md-3 col-sm-4">
                   <div class="d-flex flex-column">
                     <span class="title">زمان دوره</span>
                     <span class="info">{{ course.time }}</span>
                   </div>
                 </div>
 
-                <div class="item 2">
+                <div class="item col-md-3 col-sm-4">
                   <div class="d-flex flex-column">
                     <span class="title">بازدید دوره</span>
                     <span class="info">{{ course.viewCount }}</span>
@@ -41,16 +40,43 @@
                 </div>
               </div>
 
-
               <div class="d-flex justify-content-around btnbox flex-fill flex-wrap">
                 <router-link to="#" tag="button" id="edit" class="mybtn">ویرایش دوره</router-link>
                 <router-link to="#" tag="button" id="remove" class="mybtn">حذف دوره</router-link>
               </div>
             </div>
           </div>
-
-          <h6 v-if="!courses" class="msg">دوره ای برای نمایش وجود ندارد</h6>
-
+          <p v-if="!courses" class="msg">دوره ای برای نمایش وجود ندارد</p>
+          <p v-if="err" class="msg">خطا در برقراری ارتباط با سرور</p>
+          <p v-if="loading" class="msg">
+          <paginate
+          v-if="!loading && courses"
+            :page-count="totalPages"
+            :click-handler="page"
+            :prev-text="'قبلی'"
+            :next-text="'بعدی'"
+            :container-class="'pagination'"
+            :page-class="'page-item'"
+            :page-link-class="'page-item-link'"
+            :prev-class="'prev-class'"
+            :prev-link-class="'prev-link-class'"
+            :next-class="'next-class'"
+            :next-link-class="'next-link-class'"
+            :disabled-class="'disabled-class'"
+          >
+            <span slot="prevContent">Changed previous button</span>
+            <span slot="nextContent">Changed next button</span>
+            <span slot="breakViewContent">
+              <svg width="16" height="4" viewBox="0 0 16 4">
+                <circle fill="#999999" cx="2" cy="2" r="2"></circle>
+                <circle fill="#999999" cx="8" cy="2" r="2"></circle>
+                <circle fill="#999999" cx="14" cy="2" r="2"></circle>
+              </svg>
+            </span>
+            >
+          </paginate>
+            <span class="spinner-border spinner-border-md"></span>
+          </p>
         </div>
       </div>
     </div>
@@ -61,52 +87,126 @@
 <script>
 import adminMenu from "@/components/admin-menu.vue";
 import adminTopMenu from "@/components/admin-top-menu.vue";
-import { async } from 'q';
-import axios from 'axios'
+import { async } from "q";
+import axios from "axios";
+import paginate from "vuejs-paginate";
 export default {
-  components: { adminMenu, adminTopMenu },
-  data(){
+  components: { adminMenu, adminTopMenu, paginate },
+  data() {
     return {
-      courses : []
-    }
+      courses: [],
+      Page: 1,
+      totalPages: 1,
+      loading: false,
+      err: false
+    };
   },
-  methods:{
+  methods: {
     async page(page) {
-     let token = localStorage.getItem('auth');
-     let res = await axios({
-        method: 'post',
-        url : 'http://localhost:4000/admin/courses-info',
+      let token = localStorage.getItem("auth");
+      this.loading = true;
+      let res = await axios({
+        method: "post",
+        url: "http://localhost:4000/admin/courses-info",
         data: {
-         token,
-         page
-       }
-      })
+          token,
+          page
+        }
+      });
       this.courses = res.data.courses.docs;
-      console.log(res.data.courses);
+      this.loading = false;
     }
   },
-  async created(){
-   try {
-     let token = localStorage.getItem('auth');
-     const res = await axios({
-       method :'post',
-       url : 'http://localhost:4000/admin/courses-info',
-       data: {
-         token
-       }
-     }) 
-     this.courses = res.data.courses.docs;
-     console.log(res.data.courses);
-   } catch (err) {
-      console.log(err.response)
-   }
- }
+  async created() {
+    this.loading = true;
+    try {
+      let token = localStorage.getItem("auth");
+      const res = await axios({
+        method: "post",
+        url: "http://localhost:4000/admin/courses-info",
+        data: {
+          token
+        }
+      });
+      this.courses = res.data.courses.docs;
+      this.totalPages = res.data.courses.totalPages;
+    } catch (err) {
+      console.log(err.response);
+      this.err = true;
+    }
+    this.loading = false;
+  }
 };
- 
 </script>
 
+<style>
+.pagination {
+  margin: 20px auto;
+  justify-content: center;
+}
+.page-item {
+  background-color: #fdfdfd;
+  padding: 10px 20px;
+  -webkit-box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.03);
+  -moz-box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.03);
+  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.03);
+}
 
-<style scoped>
+.page-item-link {
+  color: #000;
+  outline: none;
+  text-decoration: none;
+}
+
+.page-item.active {
+  background-color: #7cb342;
+  box-shadow: none;
+}
+.page-item.active a {
+  color: #fff;
+  box-shadow: none;
+}
+
+.prev-class {
+  background-color: #fdfdfd;
+  padding: 10px 30px;
+  -webkit-border-top-right-radius: 5px;
+  -webkit-border-bottom-right-radius: 5px;
+  -moz-border-radius-topright: 5px;
+  -moz-border-radius-bottomright: 5px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  -webkit-box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.03);
+  -moz-box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.03);
+  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.03);
+}
+
+.prev-link-class {
+  color: #000;
+}
+
+.next-class {
+  background-color: #fdfdfd;
+  padding: 10px 30px;
+  -webkit-border-top-left-radius: 5px;
+  -webkit-border-bottom-left-radius: 5px;
+  -moz-border-radius-topleft: 5px;
+  -moz-border-radius-bottomleft: 5px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  -webkit-box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.03);
+  -moz-box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.03);
+  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.03);
+}
+.next-link-class {
+  color: #000;
+}
+.disabled-class {
+  filter: blur(1px);
+}
+</style>
+
+<style scope>
 .msg {
   color: grey;
   margin-top: 25vh;
@@ -120,8 +220,7 @@ export default {
   margin: 2px 10px;
   padding: 10px;
   border-radius: 5px;
-  border-right: 5px solid green;
-
+  border-right: 5px solid #7cb342;
 }
 .newcourse {
   padding: 5px 20px;
